6979872ea77705fd34fd59c47ed0127f941548c3
emreozhan <emreozhan71@gmail.com>
2017-03-30 02:44:50 +0300

    Threadler tek fonksiyon üstünde çalışıyor.3threadde yazıldı

--- c4.5/WindowsFormsApplication3/Form1.cs
+++ c4.5/WindowsFormsApplication3/Form1.cs
@@ -18,7 +18,7 @@ namespace WindowsFormsApplication3
         double[] KullanilanOzellikler = new double[3];
 
         double BilgiPDegeri=0;
-        double [] THREADDONUSLERI = new double[6];
+        double [] THREADDONUSLERI = new double[9];
         double deneme=0;
 
         public Form1()
@@ -82,19 +82,34 @@ namespace WindowsFormsApplication3
 
         public void THler(int[,] matris, int satirsayisi)
         {
-            int BolmeSiniri = 50;
 
-            Thread T_1A = new Thread(() => THFonksiyon1(matris, 0, satirsayisi, 50,0));
-            Thread T_1B = new Thread(() => THFonksiyon1(matris, 0, satirsayisi, 60,1));
-            Thread T_1C = new Thread(() => THFonksiyon1(matris, 0, satirsayisi, 70,2));
+            Thread T_0A = new Thread(() => THFonksiyon1(matris, 0, satirsayisi, 50,0));
+            Thread T_0B = new Thread(() => THFonksiyon1(matris, 0, satirsayisi, 60,1));
+            Thread T_0C = new Thread(() => THFonksiyon1(matris, 0, satirsayisi, 70,2));
+
+            
+            Thread T_1A = new Thread(() => THFonksiyon1(matris, 1, satirsayisi, 62,3));
+            Thread T_1B = new Thread(() => THFonksiyon1(matris, 1, satirsayisi, 63,4));
+            Thread T_1C = new Thread(() => THFonksiyon1(matris, 1, satirsayisi, 64,5));
+
+            Thread T_2A = new Thread(() => THFonksiyon1(matris, 2, satirsayisi, 5, 6));
+            Thread T_2B = new Thread(() => THFonksiyon1(matris, 2, satirsayisi, 10, 7));
+            Thread T_2C = new Thread(() => THFonksiyon1(matris, 2, satirsayisi, 19, 8));
 
 
-
+            T_0A.Start();
+            T_0B.Start();
+            T_0C.Start();
+            
             T_1A.Start();
             T_1B.Start();
             T_1C.Start();
+            
+            T_2A.Start();
+            T_2B.Start();
+            T_2C.Start();
 
-            while (deneme < 3)
+            while (deneme < 9)
             { /**threadler bittiğinde bu döngüdem kurtulur*/ }
 
 
@@ -109,7 +124,7 @@ namespace WindowsFormsApplication3
         private void THFonksiyon1(int [,] matris,int Fx,int satirsayisi, int sinir,int thid)
         {
 
-            MessageBox.Show(""+thid+"basladi");
+//            MessageBox.Show(""+thid+"basladi");
 
             double KSayi=0,SKubir=0,SKuiki=0;
             double BSayi = 0, sBubir = 0, sBuiki=0;
@@ -124,7 +139,7 @@ namespace WindowsFormsApplication3
                     else
                         SKuiki++;
                 }
-                else if (matris[sayac, Fx] > sinir)
+                else if (matris[sayac, Fx] >= sinir)
                         { BSayi++;
                         if (matris[sayac, 3] == 1)
                             sBubir++;
@@ -139,96 +154,13 @@ namespace WindowsFormsApplication3
                 (BSayi / satirsayisi) * (((-(sBubir / BSayi)) * Math.Log((sBubir / BSayi), 2)) - (((sBuiki / BSayi) * Math.Log((sBuiki / BSayi), 2))));
 
             THREADDONUSLERI[thid] = BilgiX1;
-           // MessageBox.Show("BilgiX1: "+ BilgiX1);
 
             deneme++;
 
-            MessageBox.Show("" + thid + "bitti");
+            //MessageBox.Show("" + thid + "bitti");
 
         }
    
-        private void THFonksiyon2(int[,] matris, int Fx, int satirsayisi, int sinir)
-        {
-
-            double KSayi = 0, SKubir = 0, SKuiki = 0;
-            double BSayi = 0, sBubir = 0, sBuiki = 0;
-
-            int sayac = 0;
-            while (sayac < satirsayisi)
-            {
-                if (matris[sayac, Fx] < sinir)
-                {
-                    KSayi++;
-                    if (matris[sayac, 3] == 1)
-                        SKubir++;
-                    else
-                        SKuiki++;
-                }
-                else if (matris[sayac, Fx] > sinir)
-                {
-                    BSayi++;
-                    if (matris[sayac, 3] == 1)
-                        sBubir++;
-                    else
-                        sBuiki++;
-                }
-
-                sayac++;
-            }
-
-
-            double BilgiX2 = (KSayi / satirsayisi) * (((-(SKubir / KSayi)) * Math.Log((SKubir / KSayi), 2)) - (((SKuiki / KSayi) * Math.Log((SKuiki / KSayi), 2))))
-                +
-                (BSayi / satirsayisi) * (((-(sBubir / BSayi)) * Math.Log((sBubir / BSayi), 2)) - (((sBuiki / BSayi) * Math.Log((sBuiki / BSayi), 2))));
-
-            THREADDONUSLERI[1] = BilgiX2;
-          //  MessageBox.Show("BilgiX2: " + BilgiX2);
-
-            deneme++;
-
-        }
-
-        private void THFonksiyon3(int[,] matris, int Fx, int satirsayisi, int sinir)
-        {
-
-            double KSayi = 0, SKubir = 0, SKuiki = 0;
-            double BSayi = 0, sBubir = 0, sBuiki = 0;
-
-            int sayac = 0;
-            while (sayac < satirsayisi)
-            {
-                if (matris[sayac, Fx] < sinir)
-                {
-                    KSayi++;
-                    if (matris[sayac, 3] == 1)
-                        SKubir++;
-                    else
-                        SKuiki++;
-                }
-                else if (matris[sayac, Fx] > sinir)
-                {
-                    BSayi++;
-                    if (matris[sayac, 3] == 1)
-                        sBubir++;
-                    else
-                        sBuiki++;
-                }
-
-                sayac++;
-            }
-
-
-            double BilgiX1 = (KSayi / satirsayisi) * (((-(SKubir / KSayi)) * Math.Log((SKubir / KSayi), 2)) - (((SKuiki / KSayi) * Math.Log((SKuiki / KSayi), 2))))
-                +
-                (BSayi / satirsayisi) * (((-(sBubir / BSayi)) * Math.Log((sBubir / BSayi), 2)) - (((sBuiki / BSayi) * Math.Log((sBuiki / BSayi), 2))));
-
-            THREADDONUSLERI[2] = BilgiX1;
-            // MessageBox.Show("BilgiX1: "+ BilgiX1);
-
-            deneme++;
-        }
-
-
         /** private void YEDEK(string param1, int param2)
         {
             int ii = 0;
